66b4d000dca1c8932ae8c648d9955f830a67e510
bossmonkey98 <arunambregol007@gmail.com>
2022-11-21 20:30:10 +0530

    Update quizDataSlice.ts

--- Features/quizDataSlice.ts
+++ Features/quizDataSlice.ts
@@ -22,25 +22,68 @@ const userDataSlice = createSlice({
       state.Questions = action.payload;
     },
     setAnswer(state, action) {
-      state.Questions[action.payload.id].selectedAnswer =
-        action.payload.selectedAnswer;
-      state.Questions[action.payload.id].score = action.payload.score;
-      state.Questions[action.payload.id].answered = true;
-      if (state.Questions[action.payload.id].match) {
-        let bool = false;
-        for (let i = 0; i < 4; i++) {
-          if (
-            state.Questions[action.payload.id].selectedAnswer[i].text !==
-            state.Questions[action.payload.id].answer[i].text
-          ) {
-            bool = true;
-            break;
+      if (state.Questions[action.payload.id].type !== "multi") {
+        state.Questions[action.payload.id].selectedAnswer =
+          action.payload.selectedAnswer;
+        state.Questions[action.payload.id].score = action.payload.score;
+        state.Questions[action.payload.id].answered = true;
+        if (state.Questions[action.payload.id].type === "matchTheFollowing") {
+          let bool = false;
+          for (let i = 0; i < 4; i++) {
+            if (
+              state.Questions[action.payload.id].selectedAnswer[i].text !==
+              state.Questions[action.payload.id].answer[i].text
+            ) {
+              bool = true;
+              break;
+            }
+          }
+          if (!bool) {
+            state.Questions[action.payload.id].score = 1;
+          } else {
+            state.Questions[action.payload.id].score = 0;
           }
         }
-        if (!bool) {
-          state.Questions[action.payload.id].score = 1;
-        } else {
+        if (state.Questions[action.payload.id].type === "fillBlanks") {
+          if (
+            state.Questions[action.payload.id].answer.toUpperCase() ===
+            state.Questions[action.payload.id].selectedAnswer.toUpperCase()
+          ) {
+            state.Questions[action.payload.id].score = 1;
+          } else {
+            state.Questions[action.payload.id].score = 0;
+          }
+        }
+      } else {
+        if (
+          !state.Questions[action.payload.id].selectedAnswer.includes(
+            action.payload.selectedAnswer
+          )
+        ) {
+          state.Questions[action.payload.id].selectedAnswer = [
+            ...state.Questions[action.payload.id].selectedAnswer,
+            action.payload.selectedAnswer,
+          ];
+          state.Questions[action.payload.id].answered = true;
+        }
+
+        if (state.Questions[action.payload.id].selectedAnswer.length !== 2) {
           state.Questions[action.payload.id].score = 0;
+        } else {
+          let bool = false;
+          for (let i = 0; i < 2; i++) {
+            if (
+              !state.Questions[action.payload.id].selectedAnswer.includes(
+                state.Questions[action.payload.id].answer[i]
+              )
+            ) {
+              bool = true;
+              if (bool) break;
+            } else {
+              bool = false;
+              state.Questions[action.payload.id].score = 1;
+            }
+          }
         }
       }
     },
